dfe6ff6654bc65178ee0854e35ebdd83455a0b5f
Parag Thakurdesai <parag@Cuelogic-Mac-mini.local>
2015-09-08 14:49:11 +0530

    Added listview layout file & code

---
+++ app/src/main/java/com/example/parag/myapplication/adapters/CustomAdapter.java
@@ -0,0 +1,39 @@
+package com.example.parag.myapplication.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.example.parag.myapplication.activities.MainActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by parag on 9/8/15.
+ */
+public class CustomAdapter extends BaseAdapter {
+    public  CustomAdapter(Context context,ArrayList arrayList){
+
+
+    }
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
